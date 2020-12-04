d6b91cc51af4e6c2deb695eaad916c144b3adb9e
faridrizqi46 <75464162+faridrizqi46@users.noreply.github.com>
2020-12-04 15:53:36 +0700

    Update settings.py

--- TugasPweb1/settings.py
+++ TugasPweb1/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
