b0d1caf1ca7737a1ff3f29e37578cf0222b54d63
faridrizqi46 <75464162+faridrizqi46@users.noreply.github.com>
2020-12-04 16:14:12 +0700

    update

--- TugasPweb1/settings.py
+++ TugasPweb1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2lyry&vdi7c(yg!%i4m!6ulhvwi^=o)7m_%(bv-=^mnvycz(%+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','tugaspwebtodoapp.herokuapp.com']
 
